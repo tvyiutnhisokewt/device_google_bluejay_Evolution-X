f4c602301cca1ef35f12b20f67b4c48975e5def9
nhansp <nhan.094.vn@gmail.com>
2025-07-10 16:27:17 +0700

    Initialize for Evolution 11
    Signed-off-by: nhansp <nhan.094.vn@gmail.com>

--- device-bluejay.mk
+++ device-bluejay.mk
@@ -16,8 +16,8 @@
 
 TARGET_LINUX_KERNEL_VERSION := $(RELEASE_KERNEL_BLUEJAY_VERSION)
 # Keeps flexibility for kasan and ufs builds
-TARGET_KERNEL_DIR ?= $(RELEASE_KERNEL_BLUEJAY_DIR)
-TARGET_BOARD_KERNEL_HEADERS ?= $(RELEASE_KERNEL_BLUEJAY_DIR)/kernel-headers
+TARGET_KERNEL_DIR := device/google/bluejay-kernels/$(RELEASE_KERNEL_BLUEJAY_VERSION)/tvyiutnhisokewt
+TARGET_BOARD_KERNEL_HEADERS := device/google/bluejay-kernels/$(RELEASE_KERNEL_BLUEJAY_VERSION)/tvyiutnhisokewt/kernel-headers
 
 DEVICE_PACKAGE_OVERLAYS += device/google/bluejay/bluejay/overlay
 
